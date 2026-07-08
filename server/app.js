d8b994d7ea4e14e5e760e5188bc2f9203310cb42
Vansh0769 <vanshgfeb512@gmail.com>
2026-07-08 16:24:20 +0530

    Fix Azure App Service deployment

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use("/api/students", studentRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
