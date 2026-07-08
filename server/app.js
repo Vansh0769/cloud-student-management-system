d68390113cb5b1423bdbb7353d88b3b7702f19f2
Vansh0769 <vanshgfeb512@gmail.com>
2026-07-08 19:10:08 +0530

    Connect frontend

--- server/app.js
+++ server/app.js
@@ -8,10 +8,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Cloud Student Management System API is running 🚀");
+});
+
 app.use("/api/students", studentRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
