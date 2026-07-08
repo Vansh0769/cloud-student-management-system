2342be460b79f3e164135f1104fbf2333db5fe36
Vansh0769 <vanshgfeb512@gmail.com>
2026-07-08 17:54:17 +0530

    Remove old Azure Static Web App workflow

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cloud-sms-project-d7ancvhgfycxdjhn.centralindia-01.azurewebsites.net/api",
+  baseURL: "https://sms-appservice-g6e7fkcrhabfaveq.centralindia-01.azurewebsites.net/api",
 });
 
 export default api;
