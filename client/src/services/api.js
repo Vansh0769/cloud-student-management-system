984ed09fc778fdd8809cf4b8098f374c86344588
Vansh0769 <vanshgfeb512@gmail.com>
2026-07-08 16:36:11 +0530

    Connection

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- // baseURL: "http://localhost:5000/api",
- baseURL: "cloud-sms-project-d7ancvhgfycxdjhn.centralindia-01.azurewebsites.net/api",
+  baseURL: "https://cloud-sms-project-d7ancvhgfycxdjhn.centralindia-01.azurewebsites.net/api",
 });
 
 export default api;
