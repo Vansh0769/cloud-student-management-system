e774ded383674b8030ffd4cfd1054b45b6f29e0b
Vansh0769 <vanshgfeb512@gmail.com>
2026-07-08 15:34:58 +0530

    Connect frontend with Azure backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+ // baseURL: "http://localhost:5000/api",
+ baseURL: "cloud-sms-project-d7ancvhgfycxdjhn.centralindia-01.azurewebsites.net/api",
 });
 
 export default api;
